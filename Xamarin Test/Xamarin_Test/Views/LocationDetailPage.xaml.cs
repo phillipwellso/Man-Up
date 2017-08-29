7dfc87695680635b1338b8c62abfa7d303eb839d
Phillip Wells <phillipwells@race.co.nz>
2017-08-30 10:59:28 +1200

    Location Update
    Testing can display a few other locations

--- Xamarin Test/Xamarin_Test/Views/LocationDetailPage.xaml.cs
+++ Xamarin Test/Xamarin_Test/Views/LocationDetailPage.xaml.cs
@@ -17,6 +17,8 @@ namespace Xamarin_Test.Views
     {
         Map map;
         LocationDetailViewModel viewModel;
+        Geocoder geoCoder;
+
         public LocationDetailPage()
         {
             InitializeComponent();
@@ -28,6 +30,8 @@ namespace Xamarin_Test.Views
 
             BindingContext = this.viewModel = viewModel;
 
+            geoCoder = new Geocoder();
+
             map = new Map
             {
                 IsShowingUser = true,
@@ -36,11 +40,15 @@ namespace Xamarin_Test.Views
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
 
+
             //map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(-43.905222, 171.744910), Distance.FromMiles(3))); // Santa Cruz golf course
             var zoomLevel = 16; // pick a value between 1 and 18
             var latlongdeg = 360 / (Math.Pow(2, zoomLevel));
-            map.MoveToRegion(new MapSpan(new Position(-43.905222, 171.744910), latlongdeg, latlongdeg)); // Santa Cruz golf course
-            var position = new Position(-43.905222, 171.744910); // Latitude, Longitude
+            //map.MoveToRegion(new MapSpan(new Position(-43.905222, 171.744910), latlongdeg, latlongdeg)); // Santa Cruz golf course
+            //var position = new Position(-43.905222, 171.744910); // Latitude, Longitude
+            //SetMap();
+            map.MoveToRegion(new MapSpan(new Position(-43.534652, 172.684720), latlongdeg, latlongdeg)); // Santa Cruz golf course
+            var position = new Position(-43.534652, 172.684720); // Latitude, Longitude
             var pin = new Pin
             {
                 Type = PinType.Place,
@@ -65,6 +73,31 @@ namespace Xamarin_Test.Views
         public async void ScrollUp()
         {
             await svMain.ScrollToAsync(0, 0, true);
+        }
+
+        public async void SetMap()
+        {
+            Label geocodedOutputLabel = new Label();
+            slMain.Children.Add(geocodedOutputLabel);
+            var address = "11B Pateke Place, Christchurch";
+            int retry = 0;
+            while (retry < 10)
+            {
+                try
+                {
+                    var approximateLocations = await geoCoder.GetPositionsForAddressAsync(address);
+                    foreach (var positionAddr in approximateLocations)
+                    {
+                        geocodedOutputLabel.Text += positionAddr.Latitude + ", " + positionAddr.Longitude + "\n";
+                    }
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    string error = ex.Message;
+                    retry += 1;
+                }
+            }
         }
 
     }
